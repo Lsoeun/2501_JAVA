1dd2a12b03ce4e48c5fbcd790b1b6f0f00e2f098
LeeSoeun <soeun318@naver.com>
2025-01-13 13:00:43 +0900

    코드 폴더 수정

--- 실습코드/Day01_1/src/First.java
+++
@@ -1,6 +0,0 @@
-// 첫 프로젝트 생성
-public class First {
-    public static void main(String[] args) {
-        System.out.println("우리의 첫 프로젝트");
-    }
-}
--- 실습코드/Day01_1/src/Main.java
+++
@@ -1,6 +0,0 @@
-// 시범 코드 살펴보기
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
