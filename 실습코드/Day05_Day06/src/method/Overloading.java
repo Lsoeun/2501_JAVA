74b940c9774deed617e459c554f331c8987af91e
LeeSoeun <soeun318@naver.com>
2025-01-18 17:35:54 +0900

    Day05, Day06 실습 코드 추가

--- 실습코드/Day05_Day06/src/method/Overloading.java
+++ 실습코드/Day05_Day06/src/method/Overloading.java
@@ -2,10 +2,10 @@ package method;
 
 public class Overloading {
     public void func() {
-        System.out.println("Signature is : func()");
+        System.out.println("메서드 시그니처 : func()");
     }
 
     public void func(int a) {
-        System.out.println("Signature is : func(int a)");
+        System.out.println("메서드 시그니처 : func(int a)");
     }
 }
