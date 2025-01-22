e6665f79dab7c45104911c2e126ae41f48ef5b20
LeeSoeun <soeun318@naver.com>
2025-01-22 11:29:48 +0900

    Day03 퀴즈 추가

---
+++ 실습코드/Day03/src/Quiz2.java
@@ -0,0 +1,11 @@
+public class Quiz2 {
+    public static void main(String[] args) {
+        int num = 7;
+
+        if (num % 2 == 0) {
+            System.out.println(num + "숫자는 짝수입니다.");
+        } else {
+            System.out.println(num + "숫자는 홀수입니다.");
+        }
+    }
+}
---
+++ 실습코드/Day03/src/Quiz3.java
@@ -0,0 +1,9 @@
+public class Quiz3 {
+    public static void main(String[] args) {
+        int dan = 3;
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(dan + " x " + i + " = " + (dan * i));
+        }
+    }
+}
